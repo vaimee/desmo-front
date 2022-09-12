5472c861f3e72fa20aa03115f6384d94e1bec32d
Simone Persiani <iosonopersia@gmail.com>
2022-09-12 16:28:23 +0200

    Move TDD list table code to the relevant component

--- src/app/pages/statistics-page/statistics-page.component.ts
+++ src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,69 +1,26 @@
-import {
-  AfterViewInit,
-  Component,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Subscription, tap } from 'rxjs';
-import { DesmoldSDKService } from 'src/app/services/desmold-sdk/desmold-sdk.service';
-import { StatisticsDataSource } from './statistics-datasource';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics-page.component.html',
   styleUrls: ['./statistics-page.component.css'],
 })
-export class StatisticsPageComponent
-  implements AfterViewInit, OnInit, OnDestroy
-{
-  displayedColumns: string[] = ['address', 'url', 'state', 'score'];
-  dataSource: StatisticsDataSource;
+export class StatisticsPageComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription;
-  amountOfTDDs: number = 0;
 
-  @ViewChild(MatPaginator)
-  paginator!: MatPaginator;
-
-  constructor(
-    private desmold: DesmoldSDKService,
-    private snackBar: MatSnackBar
-  ) {
-    this.dataSource = new StatisticsDataSource(this.desmold);
+  constructor(private snackBar: MatSnackBar) {
     this.subscriptions = new Subscription();
   }
 
-  private showErrorToast() {
-    this.snackBar.open('Error while loading TDDs...', 'Dismiss', {
+  showErrorToast(message: string) {
+    this.snackBar.open(message, 'Dismiss', {
       duration: 3000,
     });
   }
 
-  ngAfterViewInit() {
-    this.subscriptions.add(
-      this.paginator.page.pipe(tap(() => this.loadTDDsPage())).subscribe()
-    );
-  }
-
-  async ngOnInit(): Promise<void> {
-    this.subscriptions.add(
-      this.dataSource.error$.subscribe(() => this.showErrorToast())
-    );
-
-    this.amountOfTDDs = (
-      await this.desmold.desmoHub.getTDDStorageLength()
-    ).toNumber();
-    await this.dataSource.loadTDDs(0, this.paginator.pageSize);
-  }
-
-  async loadTDDsPage() {
-    await this.dataSource.loadTDDs(
-      this.paginator.pageIndex,
-      this.paginator.pageSize
-    );
-  }
+  ngOnInit() {}
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
