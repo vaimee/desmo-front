1dfcfc35d1e044eb355f34c931c9b8d7fd4b91ec
Simone Persiani <iosonopersia@gmail.com>
2022-09-12 16:28:23 +0200

    Move TDD list table code to the relevant component

--- src/app/components/tdd-list-table/tdd-list-table.component.ts
+++ src/app/components/tdd-list-table/tdd-list-table.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Output, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { Subscription, tap } from 'rxjs';
+import { DesmoldSDKService } from 'src/app/services/desmold-sdk/desmold-sdk.service';
+import { StatisticsDataSource } from './statistics-datasource';
 
 @Component({
   selector: 'app-tdd-list-table',
   templateUrl: './tdd-list-table.component.html',
-  styleUrls: ['./tdd-list-table.component.css']
+  styleUrls: ['./tdd-list-table.component.css'],
 })
-export class TddListTableComponent implements OnInit {
+export class TddListTableComponent implements AfterViewInit, OnInit, OnDestroy {
+  displayedColumns: string[] = ['address', 'url', 'state', 'score'];
+  dataSource: StatisticsDataSource;
+  amountOfTDDs: number = 0;
 
-  constructor() { }
+  private subscriptions: Subscription;
 
-  ngOnInit(): void {
+  @Output() errorEvent = new EventEmitter<void>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  constructor(private desmold: DesmoldSDKService) {
+    this.dataSource = new StatisticsDataSource(this.desmold);
+    this.subscriptions = new Subscription();
   }
 
+  ngAfterViewInit() {
+    this.subscriptions.add(
+      this.paginator.page.pipe(tap(() => this.loadTDDsPage())).subscribe()
+    );
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.subscriptions.add(
+      this.dataSource.error$.subscribe(() => this.errorEvent.emit())
+    );
+
+    this.amountOfTDDs = (
+      await this.desmold.desmoHub.getTDDStorageLength()
+    ).toNumber();
+    await this.dataSource.loadTDDs(0, this.paginator.pageSize);
+  }
+
+  async loadTDDsPage() {
+    await this.dataSource.loadTDDs(
+      this.paginator.pageIndex,
+      this.paginator.pageSize
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
