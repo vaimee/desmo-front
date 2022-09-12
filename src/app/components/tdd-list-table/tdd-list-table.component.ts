e58995679727103094664cb8689eeaef116d7588
Simone Persiani <iosonopersia@gmail.com>
2022-09-12 16:11:02 +0200

    Create TddListTable component

---
+++ src/app/components/tdd-list-table/tdd-list-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tdd-list-table',
+  templateUrl: './tdd-list-table.component.html',
+  styleUrls: ['./tdd-list-table.component.css']
+})
+export class TddListTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
