01ae7d170e226cf74ede7bb1ca25ab8056474479
Simone Persiani <iosonopersia@gmail.com>
2022-09-27 18:03:08 +0200

    Show user address right after the title

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { DesmoldSDKService } from 'src/app/services/desmold-sdk/desmold-sdk.service';
 
 @Component({
   selector: 'app-default',
@@ -16,5 +17,12 @@ export class DefaultComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private desmold: DesmoldSDKService
+  ) {}
+
+  public get userAddress(): string {
+    return this.desmold.userAddress;
+  }
 }
