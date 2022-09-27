0f0cf7785a2b2bd6f10d3d6d1e3785656f11279c
Simone Persiani <iosonopersia@gmail.com>
2022-09-27 17:43:59 +0200

    Add snackbar message to App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
+import { DesmoldSDKService } from './services/desmold-sdk/desmold-sdk.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'desmo-front';
+  private subscriptions: Subscription;
+
+  constructor(
+    private desmold: DesmoldSDKService,
+    private snackBar: MatSnackBar
+  ) {
+    this.subscriptions = new Subscription();
+  }
+
+  async ngOnInit(): Promise<void> {
+    await this.desmold.isReady;
+
+    this.subscriptions.add(
+      this.desmold.accountsChanged.subscribe(() => {
+        this.snackBar.open('The account was successfully changed.', 'Dismiss', {
+          duration: 3000,
+        });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
