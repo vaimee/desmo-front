d7e74296932c60c4d8bc18f3ecbc98448051836a
Simone Persiani <iosonopersia@gmail.com>
2022-09-28 14:38:49 +0200

    Update TransactionViewer component

--- src/app/components/transaction-viewer/transaction-viewer.component.ts
+++ src/app/components/transaction-viewer/transaction-viewer.component.ts
@@ -6,7 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { Subscription } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 import { DesmoldSDKService } from 'src/app/services/desmold-sdk/desmold-sdk.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { OperationType } from '@vaimee/desmold-sdk';
@@ -27,45 +27,70 @@ export class TransactionViewerComponent
 {
   private readonly CACHE_KEY: string = 'transactionList';
   displayedColumns: string[] = ['operation', 'hash', 'sent'];
-  tableData: ITransaction[];
+  txDict: Record<string, ITransaction[]>;
   dataSource: MatTableDataSource<ITransaction>;
+  private subscriptions: Subscription;
+  private currentUserAddress = '';
+  public txListSize = 0;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  constructor(private desmold: DesmoldSDKService) {
+    // Check the cache for pre-existing data or initialise with an empty object:
+    const txDictStr: string = localStorage.getItem(this.CACHE_KEY) ?? '{}';
+    this.txDict = JSON.parse(txDictStr) as Record<string, ITransaction[]>;
+
+    this.dataSource = new MatTableDataSource<ITransaction>([]);
+    this.subscriptions = new Subscription();
   }
 
-  private subscriptions: Subscription = new Subscription();
-
-  constructor(private desmold: DesmoldSDKService) {
-    // Check the cache for pre-existing data or initialise with an empty list:
-    const txList: string = sessionStorage.getItem(this.CACHE_KEY) ?? '[]';
-    this.tableData = JSON.parse(txList) as ITransaction[];
-    this.dataSource = new MatTableDataSource<ITransaction>(this.tableData);
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.subscriptions.add(
+      this.paginator.page.pipe(tap(() => this.showDataPage())).subscribe()
+    );
   }
 
   async ngOnInit(): Promise<void> {
-    await this.desmold.connect();
+    await this.desmold.isReady;
 
+    if (!this.desmold.desmoHub.isListening) {
+      await this.desmold.desmoHub.startListeners();
+    }
+
+    this.currentUserAddress = this.desmold.userAddress;
+    this.updateTxList();
+
+    // Update the table when the selected account changes:
+    this.subscriptions.add(
+      this.desmold.accountsChanged.subscribe(({ newValue }) => {
+        this.currentUserAddress = newValue;
+        this.updateTxList();
+      })
+    );
+
+    // Transactions subscription:
     this.subscriptions.add(
       this.desmold.desmoHub.transactionSent$.subscribe((tx) => {
-        this.tableData.unshift({
-          invokedOperation: this._fromOperationTypeToString(
-            tx.invokedOperation
-          ),
+        this.txDict[this.currentUserAddress].unshift({
+          invokedOperation: this.fromOperationTypeToString(tx.invokedOperation),
           hash: tx.hash,
           sent: tx.sent,
         } as ITransaction);
-        this.dataSource = new MatTableDataSource<ITransaction>(this.tableData);
 
-        // Save new data inside the cache:
-        sessionStorage.setItem(this.CACHE_KEY, JSON.stringify(this.tableData));
+        this.updateTxList();
       })
     );
   }
 
-  private _fromOperationTypeToString(opType: OperationType): string {
+  public clearCache(): void {
+    if (this.txDict[this.currentUserAddress] !== undefined) {
+      this.txDict[this.currentUserAddress] = [];
+    }
+    this.updateTxList();
+  }
+
+  private fromOperationTypeToString(opType: OperationType): string {
     switch (opType) {
       case OperationType.registerTDD:
         return 'Register TDD';
@@ -80,11 +105,25 @@ export class TransactionViewerComponent
     }
   }
 
-  clearCache(): void {
-    this.tableData = [];
-    this.dataSource = new MatTableDataSource<ITransaction>(this.tableData);
-    // Save new data inside the cache:
-    sessionStorage.setItem(this.CACHE_KEY, JSON.stringify(this.tableData));
+  private updateTxList(): void {
+    // We must be sure that txDict contains an entry for
+    // the newly selected user address:
+    if (this.txDict[this.currentUserAddress] === undefined) {
+      this.txDict[this.currentUserAddress] = [];
+    }
+    this.txListSize = this.txDict[this.currentUserAddress].length;
+
+    this.showDataPage();
+
+    // Update the cache:
+    localStorage.setItem(this.CACHE_KEY, JSON.stringify(this.txDict));
+  }
+
+  private showDataPage() {
+    const start = this.paginator.pageIndex * this.paginator.pageSize;
+    const end = start + this.paginator.pageSize;
+    const data = this.txDict[this.currentUserAddress].slice(start, end);
+    this.dataSource = new MatTableDataSource<ITransaction>(data);
   }
 
   ngOnDestroy(): void {
