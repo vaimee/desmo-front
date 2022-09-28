b186809e12c31abef6694cc42706f30cfd9611b4
Simone Persiani <iosonopersia@gmail.com>
2022-09-28 17:04:56 +0200

    Update routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StatisticsPageComponent } from './pages/statistics-page/statistics-page.component';
 import { QueryPageComponent } from './pages/query-page/query-page.component';
+import { NotConnectedPageComponent } from './pages/not-connected-page/not-connected-page.component';
+import { HasMetamaskGuard } from './guards/has-metamask/has-metamask.guard';
+import { IsLoggedInGuard } from './guards/is-logged-in/is-logged-in.guard';
+import { IsVivianiChainGuard } from './guards/is-viviani-chain/is-viviani-chain.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: DefaultComponent,
     children: [
-      { path: '', component: StatisticsPageComponent },
-      { path: 'tdd', component: TddManagerPageComponent },
-      { path: 'query', component: QueryPageComponent },
+      {
+        path: '',
+        component: StatisticsPageComponent,
+        canActivate: [HasMetamaskGuard, IsVivianiChainGuard],
+      },
+      {
+        path: 'tdd',
+        component: TddManagerPageComponent,
+        canActivate: [HasMetamaskGuard, IsVivianiChainGuard, IsLoggedInGuard],
+      },
+      {
+        path: 'query',
+        component: QueryPageComponent,
+        canActivate: [HasMetamaskGuard, IsVivianiChainGuard, IsLoggedInGuard],
+      },
+      {
+        path: 'notconnected',
+        component: NotConnectedPageComponent,
+      },
     ],
   },
 ];
