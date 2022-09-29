c8de048587c5715e191811129950c7be2b71d2f9
Simone Persiani <iosonopersia@gmail.com>
2022-09-29 17:44:28 +0200

    Don't handle errors in the Statistics page

--- src/app/pages/statistics-page/statistics-page.component.ts
+++ src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics-page.component.html',
   styleUrls: ['./statistics-page.component.css'],
 })
-export class StatisticsPageComponent {
-  constructor(private snackBar: MatSnackBar) {}
-
-  showErrorToast(message: string) {
-    this.snackBar.open(message, 'Dismiss', {
-      duration: 3000,
-    });
-  }
-}
+export class StatisticsPageComponent {}
