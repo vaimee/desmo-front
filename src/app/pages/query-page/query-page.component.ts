3f3fb64597e57599a1bd8ce14d6ef22ac6cd9a17
Simone Persiani <iosonopersia@gmail.com>
2022-10-01 10:39:17 +0200

    Implement checkpoint for query execution resuming

--- src/app/pages/query-page/query-page.component.ts
+++ src/app/pages/query-page/query-page.component.ts
@@ -1,6 +1,6 @@
 import { DesmoldSDKService } from 'src/app/services/desmold-sdk/desmold-sdk.service';
-import { Component, OnInit } from '@angular/core';
-import IQuery, { defaultIQuery } from 'src/app/interface/IQuery';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import IQuery from 'src/app/interface/IQuery';
 import { Subscription, firstValueFrom } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import {
@@ -11,6 +11,17 @@ import {
   defaultIResultTable,
 } from 'src/app/interface/IResult';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Bytes } from 'ethers';
+import { MatDialog } from '@angular/material/dialog';
+import { QueryResumeDialogComponent } from 'src/app/components/query-resume-dialog/query-resume-dialog.component';
+
+interface IQueryState {
+  executing: boolean;
+  checkPoint: number;
+
+  query?: IQuery;
+  requestID?: Bytes;
+}
 
 @Component({
   selector: 'app-query-page',
@@ -26,17 +37,31 @@ export class QueryPageComponent implements OnInit {
 
   stepperIndex = 0;
 
-  private query: IQuery;
   private subscriptions: Subscription;
+  private queryState: IQueryState = {
+    executing: false,
+    checkPoint: 0,
+  };
+  private readonly CACHE_KEY = 'queryState';
 
   constructor(
     private desmold: DesmoldSDKService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    private cd: ChangeDetectorRef
   ) {
-    this.query = defaultIQuery();
     this.result = defaultIResult();
     this.resultTable = defaultIResultTable();
     this.subscriptions = new Subscription();
+
+    const cachedValue = localStorage.getItem(this.CACHE_KEY);
+    if (cachedValue === null) {
+      // Cache miss
+      this.resetQueryState();
+    } else {
+      // Cache hit
+      this.queryState = JSON.parse(cachedValue) as IQueryState;
+    }
   }
 
   public async ngOnInit(): Promise<void> {
@@ -65,47 +90,126 @@ export class QueryPageComponent implements OnInit {
         }
       })
     );
+
+    if (this.queryState.executing === true) {
+      const dialogRef = this.dialog.open(QueryResumeDialogComponent, {
+        disableClose: true,
+        autoFocus: true,
+      });
+
+      dialogRef.afterClosed().subscribe(async (result) => {
+        if (result) {
+          await this.continueQueryExecution();
+        } else {
+          this.resetQueryState();
+        }
+      });
+    }
   }
 
-  public async executeQuery(query: IQuery) {
+  public async executeQuery(query: IQuery): Promise<void> {
+    if (this.queryState.executing === true) {
+      throw new Error(
+        "Cannot execute query until the current one isn't finished."
+      );
+    }
+
     this.result.loading = true;
     this.resultTable = defaultIResultTable();
 
-    this.query = query;
-    await this.computeQuery();
+    const requestID: Bytes = await this.executeFirstPhase();
+    this.saveFirstCheckPoint(query, requestID);
+
+    await this.executeSecondPhase(query, requestID);
+
+    this.result.loading = false;
   }
 
-  private async computeQuery() {
+  private saveFirstCheckPoint(query: IQuery, requestID: Bytes) {
+    this.queryState.executing = true;
+    this.queryState.checkPoint = 1;
+    this.queryState.query = query;
+    this.queryState.requestID = requestID;
+
+    // Persist the current state:
+    localStorage.setItem(this.CACHE_KEY, JSON.stringify(this.queryState));
+  }
+
+  private async continueQueryExecution() {
+    if (this.queryState.checkPoint === 0) {
+      throw new Error(
+        "Cannot continue executing a query that didn't even start."
+      );
+    } else if (this.queryState.checkPoint === 1) {
+      if (
+        this.queryState.query === undefined ||
+        this.queryState.requestID === undefined
+      ) {
+        throw new Error(
+          'Invalid query state: query or requestID missing at first checkpoint.'
+        );
+      }
+      const query: IQuery = this.queryState.query;
+      const requestID: Bytes = this.queryState.requestID;
+
+      this.result.loading = true;
+      this.resultTable = defaultIResultTable();
+
+      this.stepperIndex = 0;
+      // Apparently, this is needed otherwise the previous line has no visual effect:
+      this.cd.detectChanges();
+
+      await this.executeSecondPhase(query, requestID);
+
+      this.result.loading = false;
+    }
+  }
+
+  private async executeFirstPhase(): Promise<Bytes> {
     this.stepperIndex = 0;
     const eventPromise = firstValueFrom(this.desmold.desmoHub.requestID$);
     await this.desmold.desmoHub.getNewRequestID();
     const event = await eventPromise;
     this.result.requestId = event.requestID;
-    this.notifySentTransaction('new request ID received');
+    this.notifySentTransaction('New request ID obtained.');
 
+    return event.requestID;
+  }
+
+  private async executeSecondPhase(
+    query: IQuery,
+    requestID: Bytes
+  ): Promise<void> {
     this.stepperIndex = 1;
-    const queryToSend: string = this.encodeQuery(this.query);
+    const queryToSend: string = this.encodeQuery(query);
     await this.desmold.desmo.buyQuery(
-      this.result.requestId,
+      requestID,
       queryToSend,
       environment.iExecDAppAddress
     );
-    this.notifySentTransaction('Query successfully sent');
+    this.notifySentTransaction('Query successfully sent.');
 
     this.stepperIndex = 2;
-    const { result, type } = await this.desmold.desmo.getQueryResult();
-    if (this.stepperIndex === 0) {
-      this.notifySentTransaction('Query failed');
+    try {
+      const { result, type } =
+        await this.desmold.desmo.getQueryResult();
+
+      this.notifySentTransaction('Query result received.');
+      const elapsedTime = this.elapsed(this.start);
+      this.queryCompleted(query, result, type, elapsedTime);
+      this.stepperIndex = 4;
+    } catch {
+      this.notifySentTransaction('Query execution failed.');
       this.resultReset();
-      return;
+    } finally {
+      // Query execution terminated (successfully or not).
+      // Let's reset the cached query state!
+      this.resetQueryState();
     }
-    this.notifySentTransaction('Query result received');
-    const elapsedTime = this.elapsed(this.start);
-    this.queryCompleted(result, type, elapsedTime);
-    this.stepperIndex = 4;
   }
 
   private queryCompleted(
+    query: IQuery,
     value: number | string,
     type: QueryResultTypes,
     elapsedTime: number
@@ -115,14 +219,25 @@ export class QueryPageComponent implements OnInit {
     this.result.data.value = value;
     this.result.data.type = type;
     this.result.elapsedTime = elapsedTime;
-    this.result.query = this.query;
+    this.result.query = query;
     const resultTable: IResultTable = {
-      property: this.query.property.identifier,
+      property: query.property.identifier,
       value: value,
-      unit: this.query.property.unit,
+      unit: query.property.unit,
       time: this.result.elapsedTime,
     };
     this.resultTable.push(resultTable);
+  }
+
+  private resetQueryState() {
+    const initialQueryState: IQueryState = {
+      executing: false,
+      checkPoint: 0,
+    };
+    this.queryState = initialQueryState;
+
+    // Persist the current state:
+    localStorage.setItem(this.CACHE_KEY, JSON.stringify(initialQueryState));
   }
 
   private resultReset(): void {
